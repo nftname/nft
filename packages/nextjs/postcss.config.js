59fdb69a81ec3418e9344f7991ce4a073a3db3c3
nftname <nftnamemarket@gmail.com>
2025-12-23 17:57:12 +0000

    Update project files

--- packages/nextjs/postcss.config.js
+++ packages/nextjs/postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {
+      // Suppress @property warning - it's a valid CSS feature supported in modern browsers
+      // The warning comes from daisyUI's internal CSS and can be safely ignored
+      suppressWarnings: ['@property']
+    },
   },
 };
